826cead55e664eef4a1cd734a6f6a137d6a99072
Hubert Łępicki <hubert.lepicki@amberbit.com>
2015-02-16 12:41:19 +0100

    allow running migrations from within project

--- lib/audit_trigger_rails/engine.rb
+++ lib/audit_trigger_rails/engine.rb
@@ -19,5 +19,13 @@ module AuditTriggerRails
     initializer "audit_trigger_rails.add_middleware" do |app|
       app.middleware.use AuditTriggerRails::Middleware
     end
+
+    initializer :append_migrations do |app|
+      unless app.root.to_s.match root.to_s
+        config.paths["db/migrate"].expanded.each do |expanded_path|
+          app.config.paths["db/migrate"] << expanded_path
+        end
+      end
+    end
   end
 end
